a939002f1f08ac3b60ebc9bc8e01b460f35b4360
Lara <kiepsel@MacBook-Air-von-Lara.local>
2024-02-05 11:07:47 +0100

    integration

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ from dotenv import load_dotenv
 import weather
 import xlwings as xw
 import Karten
-
+import lecturedata
+import minigames
+from datetime import date, datetime, timedelta
+import random
+import discord.utils
+from discord.ext import commands,tasks
+message = int
 
 load_dotenv()
 TOKEN: Final[str]= os.getenv('DISCORD_TOKEN')
@@ -51,15 +57,126 @@ async def tarot_with_cards(interaction: discord.Interaction,amount:int):
         await interaction.channel.send(embeds=x)
     else:
         await interaction.response.send_message (f'Please choose between 1 and 3 cards', ephemeral=True)
+
+# lecture command for getting todays lecture plan
+@bot.tree.command(name="lecture",description="Check the lecture plan!")
+async def lecture(interaction: discord.ui.Button):
+    global lecturecounter
+    lecturecounter = 0
+    printing_lecture = lecturedata.lecture_data(date.today())
+    global message    
+    message = await interaction.response.send_message(embed=printing_lecture, view = lecturedata.embed_buttons())
+
+# roll-a-dice
+@bot.tree.command(name="roll-a-dice",description="You have a gambling addiction and love discord? Try both at the same time!")
+async def rolladice(interaction:discord.Interaction):
+  await interaction.response.send_message(embed=minigames.dice_embed(minigames.roll()))
+
+# password generator with user input
+@bot.tree.command(name="generate_password",description="Generate a password with the length of your choice!")
+async def generatepassword(interaction: discord.Interaction,length :int):
+    member = interaction.user
+    try: 
+            await member.send(minigames.password(length))
+            await interaction.response.send_message(f"Password has been sent to {member.global_name}.", ephemeral=True)
+    except:
+            await interaction.response.send_message(f"Wished password-length is too long, sorry :(",ephemeral = True)
+
+# counting game
+@bot.tree.command(name="start_counting",description="Count up but stay concentrated :)")
+async def start_counting(interaction:discord.Interaction):
+        global exit2
+        exit2 = 0
+        # getting channel
+        channel_id = interaction.channel.id
+        channel = bot.get_channel(channel_id)
+        global author
+        author = interaction.user
+        await interaction.response.send_message("Count up from **1**. *Wrong answers have consequences...*")
+        def check(message):
+             return message.author and message.channel and message.content.isdigit()
+        preNumber = 1
+        # checking for next number
+        while True:
+                user_guess = await bot.wait_for('message', check=check,timeout=None)
+                number = int(user_guess.content)
+                if user_guess.author != author:
+                    if number == preNumber + 1:
+                        await user_guess.add_reaction("✅")
+                        preNumber = number
+                        author = user_guess.author
+                    else:
+                        await user_guess.add_reaction("❌")
+                        preNumber = preNumber - 5
+                        await channel.send(f"Uh ohhh, new number is **{preNumber}**. Fix your mistake <@{user_guess.author.id}> 😉")
+                        if preNumber <= 0:
+                            await channel.send(f"You lost...try again 😭")
+                            exit2 = 1
+                    if exit2 == 1:
+                        break
+                else: 
+                     await channel.send("Not so greedy! This is a team-game.")
+                        
+
+# guess the number game
+@bot.tree.command(name="guess_the_number",description="Guess a number between 'start' and 'length'!")
+async def guess_the_number(interaction:discord.Interaction,start: int, end:int):
+        global exit 
+        exit = 0
+        channel_id = interaction.channel.id
+        channel = bot.get_channel(channel_id)
+        randNumber = random.randint(start, end)
+        await interaction.response.send_message(f"Guess the number between {start} and {end}!")
+        def check(message):
+             return message.author and message.channel and message.content.isdigit()
+        # checking for next number
+        while True:
+            try:
+                user_guess = await bot.wait_for('message', check=check,timeout=30)
+                guess = int(user_guess.content)
+                if guess == randNumber:
+                    await user_guess.add_reaction("✅")
+                    exit = 1
+                else:
+                    await user_guess.add_reaction("❌")
+                if exit == 1:
+                    break
+            except TimeoutError:
+                await channel.send(f"You lose...the number was {randNumber}.")
+                break
+
+# printing lecture plan of the next day ever 24 hours
+@tasks.loop(hours=24)
+async def regular_lecture():
+    # Get the current time
+    now = datetime.now()
+    # sends message if hour ist 18
+    if now.hour == 11:
+        channel_id = 1201958342990508043  
+        channel = bot.get_channel(channel_id)
+        try:    
+                message = await channel.fetch_message(
+                channel.last_message_id) 
+                await message.delete()
+        except Exception as e:
+                print(e)
+        await channel.send(embed=lecturedata.regular_data(date.today()))   
+        print("done")
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now ready!')
     try:
         synced =await bot.tree.sync()
         print(f"Synced {len(synced)} command(s)")
+        regular_lecture.start()
+        ws= xw.Book("plzdoc.xlsx").sheets["Sheet1"]
     except Exception as e:
         print(e)
-    ws= xw.Book("plzdoc.xlsx").sheets["Sheet1"]
+    
+
+
+
 #main entry point
 def main()-> None:
     bot.run(token=TOKEN)
